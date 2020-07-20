6f38ec2ed84a84d34d12c16759a47b0044db2d7c
picolov <picolov@dkatalis.com>
2020-07-20 14:35:32 +0700

    initial commit

---
+++ config/functions/bootstrap.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * An asynchronous bootstrap function that runs before
+ * your application gets started.
+ *
+ * This gives you an opportunity to set up your data model,
+ * run jobs, or perform some special logic.
+ *
+ * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#bootstrap
+ */
+
+module.exports = async () => {
+  process.nextTick(() => {
+    const io = require("socket.io")(strapi.server);
+    io.on("connection", function (socket) {
+      console.log("a user connected");
+      // send message on user connection
+      socket.emit("hello", JSON.stringify({ message: "Hello user" }));
+
+      // listen for user diconnect
+      socket.on("disconnect", () => console.log("a user disconnected"));
+    });
+    strapi.io = io; // register socket io inside strapi main object to use it globally anywhere
+  });
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '78d8db81-3f41-4d0c-bd9c-1d53186d109b'
+};
